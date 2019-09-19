d8748ffdaf54f93de2ca8dc55ff2961fda5eab7a
lisop <lisophorm@gmail.com>
2019-09-19 10:54:56 +0100

    first component implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LicenseManager} from 'ag-grid-enterprise';
+import 'ag-grid-enterprise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'betbuilder-scrollers';
+
+  columnDefs = [
+    {headerName: 'PartitionKey', field: 'PartitionKey', sortable: true, filter: true},
+    {headerName: 'RowKey', field: 'RowKey', sortable: true, filter: true},
+    {headerName: 'Timestamp', field: 'Timestamp', sortable: true, filter: true},
+    {headerName: 'Client', field: 'Client', sortable: true, filter: true},
+    {headerName: 'Competition', field: 'Competition', sortable: true, filter: true},
+    {headerName: 'Match', field: 'Match', sortable: true, filter: true},
+    {headerName: 'TimeRequested', field: 'TimeRequested', sortable: true, filter: true},
+    {headerName: 'Acceptance', field: 'Acceptance', sortable: true, filter: true},
+    {headerName: 'Stake', field: 'Stake', sortable: true, filter: true},
+    {headerName: 'Odds', field: 'Odds', sortable: true, filter: true},
+    {headerName: 'Liability', field: 'Liability', sortable: true, filter: true},
+    {headerName: 'Status', field: 'Status', sortable: true, filter: true},
+    {headerName: 'TimeSettled', field: 'TimeSettled', sortable: true, filter: true},
+    {headerName: 'Description', field: 'Description', sortable: true, filter: true},
+    {headerName: 'BetSlipUid', field: 'BetSlipUid', sortable: true, filter: true}
+  ];
+
+  rowData: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    LicenseManager.setLicenseKey('no license');
+    this.http.get('assets/static/Mock_BetSlipSummarScrollerData.json').toPromise()
+      .then(result => {
+        this.rowData = result;
+        console.log('result', result);
+      }, fail => {
+        console.log('fail', fail);
+      });
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TableComponent} from './table/table.component';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AgGridModule} from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:
+    [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
